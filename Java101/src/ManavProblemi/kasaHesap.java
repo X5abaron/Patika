b252076c4a1deb965f39dccc0f5e7244bc4cd3cd
X5abaron <ahmeetcak1r@icloud.com>
2024-02-04 17:25:34 +0300

    .

--- Java101/src/ManavProblemi/kasaHesap.java
+++ Java101/src/ManavProblemi/kasaHesap.java
@@ -6,17 +6,17 @@ public class kasaHesap {
 
         Scanner input = new Scanner(System.in);
         System.out.print("Kaç kilogram armut aldığınızı giriniz:");
-        armut = input.nextDouble();
+        armut = input.nextDouble() * 2.14;
         System.out.print("Kaç kilogram elma aldığınızı giriniz:");
-        elma = input.nextDouble();
+        elma = input.nextDouble() * 3.67;
         System.out.print("Kaç kilogram domates aldığınızı giriniz:");
-        domates = input.nextDouble();
+        domates = input.nextDouble() * 1.11;
         System.out.print("Kaç kilogram muz aldığınızı giriniz:");
-        muz = input.nextDouble();
+        muz = input.nextDouble() * 0.95;
         System.out.print("Kaç kilogram patlıcan aldığınızı giriniz:");
-        patlican = input.nextDouble();
+        patlican = input.nextDouble() * 5;
 
-        double tutar = (2.14 * armut) + (3.67 * elma) + (1.11 * domates) + (0.95 * muz) + (5 * patlican);
+        double tutar = armut + elma + domates + muz + patlican;
         System.out.print("Alışveriş tutarınız:"+tutar);
     }
 }
